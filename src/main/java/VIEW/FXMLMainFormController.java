e51459a0fb1b515cd904b17ed9762fc9e73dcbfb
Kovalenko Vyacheslav <oxx01092014@gmail.com>
2018-05-14 21:07:04 +0300

    Added MouseClicked function in gridPaneTop

--- src/main/java/VIEW/FXMLMainFormController.java
+++ src/main/java/VIEW/FXMLMainFormController.java
@@ -1,9 +1,14 @@
 package VIEW;
 
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import java.io.File;
@@ -14,6 +19,7 @@ import java.util.ResourceBundle;
 
 public class FXMLMainFormController implements Initializable {
 
+    Integer columnIndex;
     @FXML
     private GridPane gridPaneTop;
 
@@ -22,17 +28,25 @@ public class FXMLMainFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        File file = new File("src/main/resources/image/SmallSun.png");
+
+        File file0 = new File("src/main/resources/image/Man.png");
+        Image image0 = new Image(file0.toURI().toString());
+        ImageView imageView0 = new ImageView();
+        imageView0.setImage(image0);
+
+        gridPaneBot.add(imageView0,0, 1);
+
+        File file = new File("src/main/resources/image/large.png");
         Image image = new Image(file.toURI().toString());
         ImageView imageView = new ImageView();
         imageView.setImage(image);
 
-        File file1 = new File("src/main/resources/image/Crystall.png");
+        File file1 = new File("src/main/resources/image/middle.png");
         Image image1 = new Image(file1.toURI().toString());
         ImageView imageView1 = new ImageView();
         imageView1.setImage(image1);
 
-        File file2 = new File("src/main/resources/image/Compas.png");
+        File file2 = new File("src/main/resources/image/small.png");
         Image image2 = new Image(file2.toURI().toString());
         ImageView imageView2 = new ImageView();
         imageView2.setImage(image2);
@@ -43,8 +57,19 @@ public class FXMLMainFormController implements Initializable {
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < gridPaneTop.getColumnConstraints().size(); j++) {
                 iv.add(new ImageView(im[new Random().nextInt(3)]));
-                gridPaneTop.add(iv.get(iv.size() - 1),j,i);
+                gridPaneTop.add(iv.get(iv.size() - 1), j, i);
             }
+        }
+
+
+        ObservableList<Node> nodes = gridPaneTop.getChildren();
+        for(Node child : nodes) {
+            child.setOnMouseClicked((EventHandler) event -> {
+                columnIndex = GridPane.getColumnIndex(child);
+                System.out.println(columnIndex);
+//                if(GridPane.getColumnIndex(child) == 0)
+
+            });
         }
     }
 }
