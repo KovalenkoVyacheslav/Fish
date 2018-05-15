538965a0b240f078baee0a1b49da1a450727d4c8
Japonezzz <Japonezzz@bigmir.net>
2018-05-15 18:44:41 +0300

    Added just a little view of fishes

--- src/main/java/VIEW/FXMLMainFormController.java
+++ src/main/java/VIEW/FXMLMainFormController.java
@@ -5,11 +5,16 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.io.File;
 import java.net.URL;
@@ -36,17 +41,17 @@ public class FXMLMainFormController implements Initializable {
 
         gridPaneBot.add(imageView0,0, 1);
 
-        File file = new File("src/main/resources/image/large.png");
+        File file = new File("src/main/resources/image/large2.png");
         Image image = new Image(file.toURI().toString());
         ImageView imageView = new ImageView();
         imageView.setImage(image);
 
-        File file1 = new File("src/main/resources/image/middle.png");
+        File file1 = new File("src/main/resources/image/middle2.png");
         Image image1 = new Image(file1.toURI().toString());
         ImageView imageView1 = new ImageView();
         imageView1.setImage(image1);
 
-        File file2 = new File("src/main/resources/image/small.png");
+        File file2 = new File("src/main/resources/image/small2.png");
         Image image2 = new Image(file2.toURI().toString());
         ImageView imageView2 = new ImageView();
         imageView2.setImage(image2);
@@ -56,10 +61,16 @@ public class FXMLMainFormController implements Initializable {
 
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < gridPaneTop.getColumnConstraints().size(); j++) {
-                iv.add(new ImageView(im[new Random().nextInt(3)]));
+                iv.add(new ImageView(im[new Random().nextInt(3)]) );
+                iv.get(iv.size() - 1).setFitWidth(60);
+                iv.get(iv.size() - 1).setFitHeight(30);
                 gridPaneTop.add(iv.get(iv.size() - 1), j, i);
+                GridPane.setValignment(iv.get(iv.size() - 1), VPos.CENTER);
+                GridPane.setHalignment(iv.get(iv.size() - 1), HPos.CENTER);
             }
         }
+
+        gridPaneTop.setGridLinesVisible(true);
 
 
         ObservableList<Node> nodes = gridPaneTop.getChildren();
@@ -73,7 +84,8 @@ public class FXMLMainFormController implements Initializable {
                 ImageView imgv = new ImageView();
                 imgv.setImage(img);
                 gridPaneBot.add(imgv,columnIndex, 1);
-
+                GridPane.setValignment(imgv, VPos.CENTER);
+                GridPane.setHalignment(imgv, HPos.CENTER);
             });
         }
     }
